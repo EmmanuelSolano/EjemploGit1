4ea635405eda56da1d7b2b5de9ac51cefe1616e2
EmmanuelSolano <emmanuelsolano33@gmail.com>
2018-03-20 10:44:20 -0600

    Prueba commit

---
+++ domain/Zapato.java
@@ -0,0 +1,30 @@
+
+package domain;
+
+
+public class Zapato {
+    private int numCalzado;
+    
+    public Zapato(){
+    }
+
+    public Zapato(int numCalzado) {
+        this.numCalzado = numCalzado;
+    }
+
+    public int getNumCalzado() {
+        return numCalzado;
+    }
+
+    public void setNumCalzado(int numCalzado) {
+        this.numCalzado = numCalzado;
+    }
+
+    @Override
+    public String toString() {
+        return "Zapato{" + "numCalzado=" + numCalzado + '}';
+    }
+    
+    
+    
+}
